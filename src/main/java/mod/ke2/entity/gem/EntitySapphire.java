e80b62f8f15849953f4a3351a34ec55266328d36
Owen Adrian <akrivus@gmail.com>
2019-04-24 15:39:52 -0500

    Stopping point for Sapphires.

--- src/main/java/mod/ke2/entity/gem/EntitySapphire.java
+++ src/main/java/mod/ke2/entity/gem/EntitySapphire.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import mod.ke2.api.EntityGem;
 import mod.ke2.api.variants.VariantHelper;
 import mod.ke2.api.variants.types.TagType;
+import mod.ke2.api.variants.types.VariantPath;
+import mod.ke2.init.Ke2Variants;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
@@ -28,6 +31,7 @@ public class EntitySapphire extends EntityGem {
 	 */
 	public static final ArrayList<TagType> SAPPHIRE_COLORS = new ArrayList<TagType>();
 	static {
+		Ke2Variants.addVariantToGem(Ke2Variants.registerVariant(new ResourceLocation("ke2:variants/sapphire/skin_default"), VariantPath.class), EntitySapphire.class);
 		EntitySapphire.SAPPHIRE_COLORS.add(new TagType(80, "ke2:sapphire.color.blue"));
 		EntitySapphire.SAPPHIRE_COLORS.add(new TagType(10, "ke2:sapphire.color.purple"));
 		EntitySapphire.SAPPHIRE_COLORS.add(new TagType(5,  "ke2:sapphire.color.pink"));
