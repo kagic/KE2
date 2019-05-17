cfa59f7af0dbba295968b7ae010d82defb308275
Owen Adrian <akrivus@gmail.com>
2019-05-17 13:55:33 -0500

    Warp pad blocks finished.

--- src/main/java/mod/ke2/init/Ke2CreativeTabs.java
+++ src/main/java/mod/ke2/init/Ke2CreativeTabs.java
@@ -8,10 +8,12 @@ import net.minecraft.item.ItemStack;
 
 public class Ke2CreativeTabs extends CreativeTabs {
 	public static final Ke2CreativeTabs GEMSTONES = new Ke2CreativeTabs(Ke2Items.GREEN_GEM_SHARD, "gemstones");
-	public static final Ke2CreativeTabs GEM_TECH = new Ke2CreativeTabs(Ke2Items.GREEN_GEM_DESTABILIZER, "gem_tech");
+	public static final Ke2CreativeTabs GEM_TECH = new Ke2CreativeTabs(Ke2Blocks.WAILING_STONE, "gem_tech");
+	private final String suffix;
 	private final Item item;
 	public Ke2CreativeTabs(Item item, String suffix) {
 		super(KAGIC.MODID + "_" + suffix);
+		this.suffix = suffix;
 		this.item = item;
 	}
 	public Ke2CreativeTabs(Block block, String suffix) {
