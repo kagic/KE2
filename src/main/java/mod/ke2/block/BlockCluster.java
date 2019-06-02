a444a6a1b6cc3644379816796ee015600d9d4ab0
Alfhilld <47833857+Alfhilld@users.noreply.github.com>
2019-06-01 21:20:59 -0400

    Adding Cluster Blocks

---
+++ src/main/java/mod/ke2/block/BlockCluster.java
@@ -0,0 +1,22 @@
+package mod.ke2.block;
+
+import mod.ke2.init.Ke2CreativeTabs;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class BlockCluster extends Block{
+	public BlockCluster() {
+		super(Material.ROCK);
+		this.setUnlocalizedName("cluster");
+    	this.setResistance(35);
+    	this.setHardness(2);
+		this.setHarvestLevel("pickaxe", 1);
+        this.setCreativeTab(Ke2CreativeTabs.GEM_TECH);
+        this.setSoundType(SoundType.METAL);        
+        this.setLightLevel(5f);
+        
+	}
+	
+	
+}
