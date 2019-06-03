4f958c9b7973e86232d09cb92d4fac239a51f143
Owen Adrian <akrivus@gmail.com>
2019-06-03 17:20:42 -0500

    Completed Twin Rutile model.

--- src/main/java/mod/ke2/client/model/gem/ModelLapisLazuli.java
+++ src/main/java/mod/ke2/client/model/gem/ModelLapisLazuli.java
@@ -10,48 +10,49 @@ public class ModelLapisLazuli extends ModelGem {
 	private ModelRenderer bipedRightWing;
 	public ModelLapisLazuli() {
 		super(0.0F, 0.0F, 64, 64, 4);
-		this.bipedHead = (new ModelRenderer(this, 0, 0)).setTextureSize(64, 128);
-		this.bipedHead.addBox(-4F, -8F, -4F, 8, 8, 8);
-		this.bipedHead.setRotationPoint(0F, 0F, 0F);
-		this.bipedHeadwear = (new ModelRenderer(this, 32, 0)).setTextureSize(64, 128);
-		this.bipedHeadwear.addBox(-4F, -8F, -4F, 8, 8, 8, 1.1F);
-		this.bipedHeadwear.setRotationPoint(0F, 0F, 0F);
-		this.bipedBody = (new ModelRenderer(this, 8, 16)).setTextureSize(64, 128);
-		this.bipedBody.addBox(-3F, 0F, -2F, 6, 12, 4);
-		this.bipedBody.setRotationPoint(0F, 0F, 0F);
-		this.bipedRightWing = (new ModelRenderer(this, 0, 32)).setTextureSize(64, 128);
+		this.bipedHead = new ModelRenderer(this, 0, 0);
+        this.bipedHead.addBox(-4.0F, -4.0F, -4.0F, 8, 8, 8, 0.0F);
+        this.bipedHead.offsetY = -0.125F;
+        this.bipedHeadwear = new ModelRenderer(this, 32, 0);
+        this.bipedHeadwear.addBox(-4.0F, -4.0F, -4.0F, 8, 8, 8, 1.1F);
+        this.bipedHead.addChild(this.bipedHeadwear);
+        this.bipedLeftArm = new ModelRenderer(this, 0, 16);
+        this.bipedLeftArm.addBox(-2.0F, 2.0F, -1.0F, 2, 12, 2, 0.0F);
+        this.bipedRightArm = new ModelRenderer(this, 0, 30);
+        this.bipedRightArm.addBox(0.0F, 2.0F, -1.0F, 2, 12, 2, 0.0F);
+        this.bipedBody = new ModelRenderer(this, 8, 16);
+        this.bipedBody.addBox(-3.0F, 2.0F, -2.0F, 6, 10, 4, 0.0F);
+		this.bipedRightWing = new ModelRenderer(this, 0, 32).setTextureSize(64, 128);
         this.bipedRightWing.addBox(0F, -2F, 2F, 10, 20, 1);
         this.bipedRightWing.setRotationPoint(-5F, 0.5F, 0F);
-		this.bipedLeftWing = (new ModelRenderer(this, 32, 32)).setTextureSize(64, 128);
+        this.bipedBody.addChild(this.bipedRightWing);
+		this.bipedLeftWing = new ModelRenderer(this, 32, 32).setTextureSize(64, 128);
 		this.bipedLeftWing.addBox(-10F, -2F, 2F, 10, 20, 1);
 		this.bipedLeftWing.setRotationPoint(-5F, 0.5F, 0F);
-		this.bipedRightArm = (new ModelRenderer(this, 0, 16)).setTextureSize(64, 128);
-		this.bipedRightArm.addBox(0F, 0F, -1F, 2, 12, 2);
-		this.bipedRightArm.setRotationPoint(0F, 0F, 0F);
-		this.bipedLeftArm = (new ModelRenderer(this, 0, 16)).setTextureSize(64, 128);
-		this.bipedLeftArm.addBox(-2F, 0F, -1F, 2, 12, 2);
-		this.bipedLeftArm.setRotationPoint(0F, 0F, 0F);
-		this.bipedRightLeg = (new ModelRenderer(this, 28, 16)).setTextureSize(64, 128);
-		this.bipedRightLeg.addBox(1F, 0F, -1F, 2, 12, 2);
-		this.bipedRightLeg.setRotationPoint(0F, 12F, 0F);
-		this.bipedLeftLeg = (new ModelRenderer(this, 28, 16)).setTextureSize(64, 128);
-		this.bipedLeftLeg.addBox(-3F, 0F, -1F, 2, 12, 2);
-		this.bipedLeftLeg.setRotationPoint(0F, 12F, 0F);
+		this.bipedBody.addChild(this.bipedLeftWing);
+        this.bipedSkirt = new ModelRenderer(this, 36, 0);
+        this.bipedSkirt.addBox(-4.0F, 12.0F, -4.0F, 8, 12, 8, 0.0F);
+        this.bipedBody.addChild(this.bipedSkirt);
+        this.bipedLeftLeg = new ModelRenderer(this, 28, 16);
+        this.bipedLeftLeg.addBox(-3.0F, 0.0F, -1.0F, 2, 12, 2, 0.0F);
+        this.bipedRightLeg = new ModelRenderer(this, 28, 30);
+        this.bipedRightLeg.addBox(1.0F, 0.0F, -1.0F, 2, 12, 2, 0.0F);
 	}
 	@Override
 	public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
 		this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entity);
 		this.bipedHead.render(scale);
-		this.bipedHeadwear.render(scale);
-		this.bipedBody.render(scale);
-		this.bipedRightArm.render(scale);
 		this.bipedLeftArm.render(scale);
-		this.bipedRightLeg.render(scale);
+		this.bipedRightArm.render(scale);
+		this.bipedBody.render(scale);
 		this.bipedLeftLeg.render(scale);
+		this.bipedRightLeg.render(scale);
 	}
 	@Override
 	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entity) {
 		super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
+		this.bipedHeadwear.rotateAngleX = 0.0F;
+		this.bipedHeadwear.rotateAngleY = 0.0F;
 		float x = 0.2617994F;
         float y = 1.0F;
         float z = -0.2617994F;
--- src/main/java/mod/ke2/client/model/gem/ModelTwinRutile.java
+++ src/main/java/mod/ke2/client/model/gem/ModelTwinRutile.java
@@ -12,44 +12,38 @@ public class ModelTwinRutile extends ModelGem {
 	public ModelTwinRutile() {
 		super(0.0F, 0.0F, 64, 64, 4);
 		this.bipedHead = new ModelRenderer(this, 0, 0);
-		this.bipedHead.addBox(-8F, -4F, -4F, 8, 10, 8);
-		this.bipedHead.setRotationPoint(0F, 0F, 0F);
+		this.bipedHead.addBox(-8.0F, -6.0F, -4.0F, 8, 8, 8);
+        this.bipedHeadwear = new ModelRenderer(this, 32, 0);
+        this.bipedHeadwear.addBox(-8.0F, -10.0F, -5.0F, 8, 4, 9, 0.1F);
+        this.bipedHead.addChild(this.bipedHeadwear);
         this.bipedOtherHead = new ModelRenderer(this, 0, 0);
-		this.bipedOtherHead.addBox(0F, -4F, -4F, 8, 10, 8);
-		this.bipedOtherHead.setRotationPoint(0F, 0F, 0F);
+		this.bipedOtherHead.addBox(0.0F, -6.0F, -4.0F, 8, 8, 8);
+        this.bipedOtherHeadwear = new ModelRenderer(this, 32, 0);
+        this.bipedOtherHeadwear.addBox(0.0F, -10.0F, -5.0F, 8, 4, 9, 0.1F);
+        this.bipedOtherHead.addChild(this.bipedOtherHeadwear);
 		this.bipedRightArm = new ModelRenderer(this, 0, 18);
-		this.bipedRightArm.addBox(-2F, 2F, -1F, 2, 12, 2);
-		this.bipedRightArm.setRotationPoint(0F, 0F, 0F);
+		this.bipedRightArm.addBox(-3.0F, -2.0F, -1.0F, 2, 14, 2);
 		this.bipedLeftArm = new ModelRenderer(this, 0, 18);
-		this.bipedLeftArm.addBox(0F, 2F, -1F, 2, 12, 2);
-		this.bipedLeftArm.setRotationPoint(0F, 0F, 0F);
+		this.bipedLeftArm.addBox(1.0F, -2.0F, -1.0F, 2, 14, 2);
 		this.bipedRightLeg = new ModelRenderer(this, 28, 18);
-		this.bipedRightLeg.addBox(1F, 0F, -1F, 2, 12, 2);
-		this.bipedRightLeg.setRotationPoint(0F, 12F, 0F);
+		this.bipedRightLeg.addBox(1.0F, -2.0F, -1.0F, 2, 14, 2);
 		this.bipedLeftLeg = new ModelRenderer(this, 28, 18);
-		this.bipedLeftLeg.addBox(-3F, 0F, -1F, 2, 12, 2);
-		this.bipedLeftLeg.setRotationPoint(0F, 12F, 0F);
+		this.bipedLeftLeg.addBox(-3.0F, -2.0F, -1.0F, 2, 14, 2);
         this.bipedLeftTorso = new ModelRenderer(this, 9, 18);
         this.bipedLeftTorso.mirror = true;
-        this.bipedLeftTorso.addBox(-5.0F, 6.0F, -2.0F, 5, 10, 4, 0.0F);
+        this.bipedLeftTorso.addBox(-5.0F, 2.0F, -2.0F, 5, 12, 4, 0.0F);
         this.bipedLeftTorso.setRotationPoint(-1.3F, -4.2F, 0.0F);
         this.bipedLeftTorso.addChild(this.bipedHead);
         this.bipedRightTorso = new ModelRenderer(this, 9, 18);
-        this.bipedRightTorso.addBox(0.0F, 6.0F, -2.0F, 5, 10, 4, 0.0F);
+        this.bipedRightTorso.addBox(0.0F, 2.0F, -2.0F, 5, 12, 4, 0.0F);
         this.bipedRightTorso.setRotationPoint(1.3F, -4.2F, 0.0F);
         this.bipedRightTorso.addChild(this.bipedOtherHead);
         this.bipedBody = new ModelRenderer(this, 8, 28);
-        this.bipedBody.addBox(-3.0F, 4.0F, -2.0F, 6, 2, 4);
-        this.bipedBody.setRotationPoint(0.0F, 6.0F, 0.0F);
+        this.bipedBody.addBox(-3.0F, 8.0F, -2.0F, 6, 2, 4);
 	}
 	@Override
 	public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
 		this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entity);
-		this.bipedOtherHead.rotateAngleX = headPitch * 0.017453292F;
-		this.bipedOtherHead.rotateAngleY = netHeadYaw * 0.017453292F;
-        this.bipedLeftTorso.rotateAngleZ = -0.1F;
-		this.bipedRightTorso.rotateAngleZ = 0.1F;
-		this.bipedHeadwear.render(scale);
 		this.bipedLeftTorso.render(scale);
 		this.bipedRightTorso.render(scale);
 		this.bipedBody.render(scale);
@@ -57,5 +51,17 @@ public class ModelTwinRutile extends ModelGem {
 		this.bipedLeftArm.render(scale);
 		this.bipedRightLeg.render(scale);
 		this.bipedLeftLeg.render(scale);
+	}
+	@Override
+	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entity) {
+		super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
+		this.bipedOtherHead.rotateAngleX = headPitch * 0.017453292F;
+		this.bipedOtherHead.rotateAngleY = netHeadYaw * 0.017453292F;
+		this.bipedOtherHeadwear.rotateAngleX = 0.0F;
+		this.bipedOtherHeadwear.rotateAngleY = 0.0F;
+		this.bipedHeadwear.rotateAngleX = 0.0F;
+		this.bipedHeadwear.rotateAngleY = 0.0F;
+		this.bipedLeftTorso.rotateAngleZ = -0.1F;
+		this.bipedRightTorso.rotateAngleZ = 0.1F;
 	}
 }
