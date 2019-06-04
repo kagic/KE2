a3bb02326ef3eae52989f94fe2c89463b82f9f51
Owen Adrian <akrivus@gmail.com>
2019-06-03 19:30:47 -0500

    Fixed outdated Steven/Connie model.

--- src/main/java/mod/ke2/client/model/human/ModelConnie.java
+++ src/main/java/mod/ke2/client/model/human/ModelConnie.java
@@ -12,29 +12,21 @@ public class ModelConnie extends ModelBiped {
 	public ModelConnie() {
 		super(0.0F, 0.0F, 64, 64);
 		this.bipedHead = new ModelRenderer(this, 0, 0);
-		this.bipedHead.addBox(-4F, -2F, -4F, 8, 8, 8);
-	    this.bipedHead.setRotationPoint(0F, 0F, 0F);
+		this.bipedHead.addBox(-4.0F, -2.0F, -4.0F, 8, 8, 8);
 	    this.bipedHeadwear = new ModelRenderer(this, 32, 0);
-		this.bipedHeadwear.addBox(-4F, -2F, -4F, 8, 8, 8, 1.1F);
-	    this.bipedHeadwear.setRotationPoint(0F, 0F, 0F);
+		this.bipedHeadwear.addBox(-4.0F, -2.0F, -4.0F, 8, 8, 8, 1.1F);
 	    this.bipedBody = new ModelRenderer(this, 8, 16);
-	    this.bipedBody.addBox(-3F, 6F, -2F, 6, 8, 4);
-	    this.bipedBody.setRotationPoint(0F, 0F, 0F);
+	    this.bipedBody.addBox(-3.0F, 6.0F, -2.0F, 6, 8, 4);
 	    this.bipedSkirt = new ModelRenderer(this, 24, 28);
-	    this.bipedSkirt.addBox(-5F, 13F, -5F, 10, 8, 10);
-	    this.bipedSkirt.setRotationPoint(0F, 0F, 0F);
+	    this.bipedSkirt.addBox(-5.0F, 13.0F, -5.0F, 10, 8, 10);;
 	    this.bipedRightArm = new ModelRenderer(this, 28, 16);
-	    this.bipedRightArm.addBox(0F, -2F, -1F, 2, 10, 2);
-	    this.bipedRightArm.setRotationPoint(0F, 8F, 0F);
+	    this.bipedRightArm.addBox(0.0F, -2.0F, -1.0F, 2, 10, 2);
 	    this.bipedLeftArm = new ModelRenderer(this, 36, 16);
-	    this.bipedLeftArm.addBox(-2F, -2F, -1F, 2, 10, 2);
-	    this.bipedLeftArm.setRotationPoint(0F, 8F, 0F);
+	    this.bipedLeftArm.addBox(-2.0F, -2.0F, -1.0F, 2, 10, 2);
 	    this.bipedRightLeg = new ModelRenderer(this, 0, 16);
-	    this.bipedRightLeg.addBox(-3F, 2F, -1F, 2, 10, 2);
-	    this.bipedRightLeg.setRotationPoint(0F, 0F, 0F);
+	    this.bipedRightLeg.addBox(-3.0F, 2.0F, -1.0F, 2, 10, 2);
 	    this.bipedLeftLeg = new ModelRenderer(this, 0, 16);
-	    this.bipedLeftLeg.addBox(1F, 2F, -1F, 2, 10, 2);
-	    this.bipedLeftLeg.setRotationPoint(0F, 0F, 0F);
+	    this.bipedLeftLeg.addBox(1.0F, 2.0F, -1.0F, 2, 10, 2);
     }
 	@Override
 	public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
--- src/main/java/mod/ke2/client/model/human/ModelSteven.java
+++ src/main/java/mod/ke2/client/model/human/ModelSteven.java
@@ -9,28 +9,21 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class ModelSteven extends ModelGem {
 	public ModelSteven() {
-		super(0.0F, 0.0F, 64, 64, 4);
+		super(0.0F, 0.0F, 64, 64);
 		this.bipedHead = new ModelRenderer(this, 0, 0);
-		this.bipedHead.addBox(-4F, 0F, -4F, 8, 8, 8);
-	    this.bipedHead.setRotationPoint(0F, 0F, 0F);
+		this.bipedHead.addBox(-4.0F, 0.0F, -4.0F, 8, 8, 8);
  		this.bipedHeadwear = new ModelRenderer(this, 32, 0);
- 		this.bipedHeadwear.addBox(-4F, 0F, -4F, 8, 8, 8, 1.1F);
- 	    this.bipedHeadwear.setRotationPoint(0F, 0F, 0F);
+ 		this.bipedHeadwear.addBox(-4.0F, 0.0F, -4.0F, 8, 8, 8, 1.1F);
 	    this.bipedBody = new ModelRenderer(this, 16, 16);
-	    this.bipedBody.addBox(-4F, 8F, -2F, 8, 8, 4);
-	    this.bipedBody.setRotationPoint(0F, 0F, 0F);
+	    this.bipedBody.addBox(-4.0F, 8.0F, -2.0F, 8, 8, 4);
 	    this.bipedRightArm = new ModelRenderer(this, 40, 16);
-	    this.bipedRightArm.addBox(-3F, 0F, -2F, 4, 8, 4);
-	    this.bipedRightArm.setRotationPoint(0F, 8F, 0F);
+	    this.bipedRightArm.addBox(-3.0F, 0.0F, -2.0F, 4, 8, 4);
 	    this.bipedLeftArm = new ModelRenderer(this, 40, 28);
-	    this.bipedLeftArm.addBox(-1F, 0F, -2F, 4, 8, 4);
-	    this.bipedLeftArm.setRotationPoint(0F, 8F, 0F);
+	    this.bipedLeftArm.addBox(-1.0F, 0.0F, -2.0F, 4, 8, 4);
 	    this.bipedRightLeg = new ModelRenderer(this, 0, 16);
-	    this.bipedRightLeg.addBox(0F, 4F, -2F, 4, 8, 4);
-	    this.bipedRightLeg.setRotationPoint(0F, 0F, 0F);
+	    this.bipedRightLeg.addBox(0.0F, 4.0F, -2.0F, 4, 8, 4);
 	    this.bipedLeftLeg = new ModelRenderer(this, 0, 16);
-	    this.bipedLeftLeg.addBox(-4F, 4F, -2F, 4, 8, 4);
-	    this.bipedLeftLeg.setRotationPoint(0F, 0F, 0F);
+	    this.bipedLeftLeg.addBox(-4.0F, 4.0F, -2.0F, 4, 8, 4);
     }
 	@Override
 	public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
