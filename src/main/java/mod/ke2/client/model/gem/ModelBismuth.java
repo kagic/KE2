e94615046fa4541e06cbfac463ae66f32d5fb674
Owen Adrian <akrivus@gmail.com>
2019-06-03 22:07:16 -0500

    Finishing Bismuth model.

--- src/main/java/mod/ke2/client/model/gem/ModelBismuth.java
+++ src/main/java/mod/ke2/client/model/gem/ModelBismuth.java
@@ -9,22 +9,23 @@ public class ModelBismuth extends ModelGem {
 		super(0.0F, 0.0F, 64, 64);
 		this.bipedHead = new ModelRenderer(this, 0, 0);
 		this.bipedHead.addBox(-4.0F, -12.0F, -4.0F, 8, 8, 8, 0.0F);
+		this.bipedHead.offsetY = -0.25F;
 		this.bipedHeadwear = new ModelRenderer(this, 32, 0);
 		this.bipedHeadwear.addBox(-4.0F, -12.0F, -4.0F, 8, 8, 8, 0.5F);
 		this.bipedHead.addChild(this.bipedHeadwear);
 		this.bipedBody = new ModelRenderer(this, 0, 16);
-		this.bipedBody.addBox(-6.0F, -4.0F, -3.0F, 12, 16, 6, 0.0F);
+		this.bipedBody.addBox(-6.0F, -8.0F, -3.0F, 12, 16, 6, 0.0F);
 		this.bipedSkirt = new ModelRenderer(this, 32, 38);
-		this.bipedSkirt.addBox(-6.0F, 8.0F, -3.0F, 12, 8, 6, 0.5F);
+		this.bipedSkirt.addBox(-6.0F, 4.0F, -3.0F, 12, 12, 6, 0.5F);
 		this.bipedBody.addChild(this.bipedSkirt);
 		this.bipedRightArm = new ModelRenderer(this, 16, 38);
-		this.bipedRightArm.addBox(-6.0F, -4.0F, -2.5F, 5, 14, 5, 0.0F);
+		this.bipedRightArm.addBox(-6.0F, -8.0F, -2.5F, 5, 16, 5, 0.0F);
 		this.bipedLeftArm = new ModelRenderer(this, 0, 38);
-		this.bipedLeftArm.addBox(1.0F, -4.0F, -2.5F, 5, 14, 5, 0.0F);
+		this.bipedLeftArm.addBox(1.0F, -8.0F, -2.5F, 5, 16, 5, 0.0F);
 		this.bipedRightLeg = new ModelRenderer(this, 48, 38);
-		this.bipedRightLeg.addBox(-6.0F, 0.0F, -2.0F, 4, 12, 4);
+		this.bipedRightLeg.addBox(-6.0F, -4.0F, -2.0F, 4, 16, 4);
 		this.bipedLeftLeg = new ModelRenderer(this, 32, 38);
-		this.bipedLeftLeg.addBox(2.0F, 0.0F, -2.0F, 4, 12, 4);
+		this.bipedLeftLeg.addBox(2.0F, -4.0F, -2.0F, 4, 16, 4);
 	}
 	@Override
 	public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
