fb1c99894eae771123ca9657903dc8c27f5f4e46
Owen Adrian <akrivus@gmail.com>
2019-06-04 21:59:59 -0500

    Finished Spodumene and Larimar models.

--- src/main/java/mod/ke2/client/model/gem/ModelGarnetHessonite.java
+++ src/main/java/mod/ke2/client/model/gem/ModelGarnetHessonite.java
@@ -4,16 +4,13 @@ import mod.ke2.client.model.ModelGem;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelDemantoid extends ModelGem {
-	public ModelRenderer bipedNeck;
-    public ModelDemantoid() {
+public class ModelGarnetHessonite extends ModelGem {
+	public ModelRenderer bipedShoulderPads;
+    public ModelGarnetHessonite() {
 		super(0.0F, 0.0F, 64, 64);
         this.bipedHead = new ModelRenderer(this, 0, 0);
-        this.bipedHead.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, 0.0F);
-        this.bipedHead.offsetY = -0.5625F;
-        this.bipedHeadwear = new ModelRenderer(this, 90, 0);
-		this.bipedHeadwear.addBox(-4.0F, -8.0F, -4.0F, 8, 16, 8, 0.5F);
-		this.bipedHead.addChild(this.bipedHeadwear);
+        this.bipedHead.addBox(-6.0F, -12.0F, -4.0F, 12, 12, 8, 0.0F);
+        this.bipedHead.offsetY = -0.625F;
         this.bipedRightArm = new ModelRenderer(this, 0, 20);
         this.bipedRightArm.addBox(-2.0F, 2.0F, -1.5F, 3, 16, 3, 0.0F);
         this.bipedRightArm.offsetY = -0.5F;
@@ -21,13 +18,19 @@ public class ModelDemantoid extends ModelGem {
         this.bipedLeftArm.addBox(-1.0F, 2.0F, -1.5F, 3, 16, 3, 0.0F);
         this.bipedLeftArm.offsetY = -0.5F;
         this.bipedBody = new ModelRenderer(this, 12, 20);
-        this.bipedBody.addBox(-4.0F, -6.0F, -3.0F, 8, 10, 6, 0.0F);
-        this.bipedNeck = new ModelRenderer(this, 0, 38);
-        this.bipedNeck.addBox(-1.5F, -9.0F, -1.5F, 3, 3, 3);
-        this.bipedBody.addChild(this.bipedNeck);
-        this.bipedSkirt = new ModelRenderer(this, 0, 38);
-        this.bipedSkirt.addBox(-5.0F, 4.0F, -5.0F, 10, 20, 10, 0.0F);
-        this.bipedBody.addChild(this.bipedSkirt);
+        this.bipedBody.addBox(-4.0F, -6.0F, -3.0F, 8, 14, 6, 0.0F);
+        this.bipedShoulderPads = new ModelRenderer(this, 0, 38);
+        this.bipedShoulderPads.addBox(-8.0F, -10.0F, -3.0F, 16, 4, 6);
+        this.bipedBody.addChild(this.bipedShoulderPads);
+        this.bipedCape = new ModelRenderer(this, 0, 38);
+        this.bipedCape.addBox(-8.0F, -6.0F, 2.0F, 16, 22, 1, 0.0F);
+        this.bipedBody.addChild(this.bipedCape);
+        this.bipedRightLeg = new ModelRenderer(this, 40, 0);
+        this.bipedRightLeg.addBox(-4.0F, 0.0F, -2.0F, 4, 16, 4, 0.0F);
+        this.bipedRightLeg.offsetY = -0.25F;
+        this.bipedLeftLeg = new ModelRenderer(this, 40, 0);
+        this.bipedLeftLeg.addBox(0.0F, 0.0F, -2.0F, 4, 16, 4, 0.0F);
+        this.bipedLeftLeg.offsetY = -0.25F;
     }
     @Override
     public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
@@ -36,11 +39,11 @@ public class ModelDemantoid extends ModelGem {
     	this.bipedRightArm.render(scale);
     	this.bipedLeftArm.render(scale);
 		this.bipedBody.render(scale);
+		this.bipedRightLeg.render(scale);
+		this.bipedLeftLeg.render(scale);
     }
 	@Override
 	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entity) {
 		super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
-		this.bipedHeadwear.rotateAngleX = 0.0F;
-		this.bipedHeadwear.rotateAngleY = 0.0F;
 	}
 }
--- src/main/java/mod/ke2/client/model/gem/ModelWatermelonTourmaline.java
+++ src/main/java/mod/ke2/client/model/gem/ModelWatermelonTourmaline.java
@@ -6,28 +6,24 @@ import net.minecraft.entity.Entity;
 
 public class ModelWatermelonTourmaline extends ModelGem {
     public ModelWatermelonTourmaline() {
-    	super(0.0F, 0.0F, 128, 64);
-        this.bipedHead = new ModelRenderer(this, 0, 0);
-        this.bipedHead.setRotationPoint(0.0F, 4.0F, 0.0F);
-        this.bipedHead.addBox(-4.0F, -1.0F, -4.0F, 8, 8, 8, 0.0F);
-        this.bipedHeadwear = new ModelRenderer(this, 32, 0);
-        this.bipedHeadwear.setRotationPoint(0.0F, 4.0F, 0.0F);
-        this.bipedHeadwear.addBox(-4.5F, -1.1F, -4.5F, 9, 9, 9, 0.0F);
-        this.bipedBody = new ModelRenderer(this, 0, 16);
-        this.bipedBody.setRotationPoint(-3.0F, 7.0F, 0.0F);
-        this.bipedBody.addBox(0.0F, 0.0F, -2.0F, 6, 9, 4, 0.0F);
-        this.bipedLeftArm = new ModelRenderer(this, 20, 29);
-        this.bipedLeftArm.setRotationPoint(-2.0F, 7.0F, 0.0F);
-        this.bipedLeftArm.addBox(-2.0F, 0.0F, -1.0F, 2, 8, 2, 0.0F);
-        this.bipedRightArm = new ModelRenderer(this, 28, 29);
-        this.bipedRightArm.setRotationPoint(0.0F, 7.0F, 0.0F);
-        this.bipedRightArm.addBox(0.0F, 0.0F, -1.0F, 2, 8, 2, 0.0F);
-        this.bipedLeftLeg = new ModelRenderer(this, 8, 29);
-        this.bipedLeftLeg.setRotationPoint(1.5F, 16.0F, 0.0F);
-        this.bipedLeftLeg.addBox(-1.0F, 4.0F, -1.0F, 2, 8, 2, 0.0F);
-        this.bipedRightLeg = new ModelRenderer(this, 0, 29);
-        this.bipedRightLeg.setRotationPoint(-1.5F, 16.0F, 0.0F);
-        this.bipedRightLeg.addBox(-1.0F, 4.0F, -1.0F, 2, 8, 2, 0.0F); 
+    	super(0.0F, 0.0F, 144, 80);
+		this.bipedHead = new ModelRenderer(this, 0, 0);
+		this.bipedHead.addBox(-4.0F, -12.0F, -4.0F, 8, 8, 8);
+		this.bipedHead.offsetY = -1.0F;
+		this.bipedBody = new ModelRenderer(this, 24, 22);
+		this.bipedBody.addBox(-6.0F, -16.0F, -4.0F, 12, 20, 8);
+		this.bipedRightArm = new ModelRenderer(this, 64, 22);
+		this.bipedRightArm.addBox(-7.0F, 0.0F, -3.0F, 6, 20, 6);
+		this.bipedRightArm.offsetY = -1.0F;
+		this.bipedLeftArm = new ModelRenderer(this, 64, 48);
+		this.bipedLeftArm.addBox(1.0F, 0.0F, -3.0F, 6, 20, 6);
+		this.bipedLeftArm.offsetY = -1.0F;
+		this.bipedRightLeg = new ModelRenderer(this, 0, 22);
+		this.bipedRightLeg.addBox(-6.0F, 0.0F, -3.0F, 6, 20, 6);
+		this.bipedRightLeg.offsetY = -0.5F;
+		this.bipedLeftLeg = new ModelRenderer(this, 0, 48);
+		this.bipedLeftLeg.addBox(0.0F, 0.0F, -3.0F, 6, 20, 6);
+		this.bipedLeftLeg.offsetY = -0.5F;
     }
     @Override
     public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
