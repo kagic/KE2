448ea6efa0b54e25215ca6d35d21d621124fefb7
Owen Adrian <akrivus@gmail.com>
2019-06-08 21:24:48 -0500

    Fixing potion icon links.

--- src/main/java/mod/ke2/potion/PotionCholeric.java
+++ src/main/java/mod/ke2/potion/PotionCholeric.java
@@ -18,13 +18,13 @@ public class PotionCholeric extends Potion {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void renderInventoryEffect(PotionEffect effect, Gui gui, int x, int y, float z) {
-	    Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("ke2:textures/effects/choleric.png"));
+	    Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("ke2:textures/potions/choleric.png"));
 		Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
 	}
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void renderHUDEffect(PotionEffect effect, Gui gui, int x, int y, float z, float alpha) {
-	    Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("ke2:textures/effects/choleric.png"));
+	    Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("ke2:textures/potions/choleric.png"));
 		Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
 	}
 }
