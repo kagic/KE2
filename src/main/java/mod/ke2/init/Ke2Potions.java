f76631333c43496dc5d866d82a0ec5af64a008c9
Owen Adrian <akrivus@gmail.com>
2019-06-08 20:24:15 -0500

    Added Potion of Temperment.

--- src/main/java/mod/ke2/init/Ke2Potions.java
+++ src/main/java/mod/ke2/init/Ke2Potions.java
@@ -1,16 +1,30 @@
 package mod.ke2.init;
 
 import mod.ke2.potion.PotionTempermental;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionHelper;
+import net.minecraft.potion.PotionType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class Ke2Potions {
-	public static final PotionTempermental POTION_TEMPERMENTAL = new PotionTempermental();
+	public static final PotionTempermental TEMPERMENTAL = new PotionTempermental();
+	public static final PotionType TEMPERMENT = new PotionType(new PotionEffect[] { new PotionEffect(Ke2Potions.TEMPERMENTAL, 3600) });
 	public static void register() {
-		registerPotion(POTION_TEMPERMENTAL, new ResourceLocation("ke2:tempermental"));
+		registerPotion(TEMPERMENTAL, new ResourceLocation("ke2:tempermental"));
+		registerPotionType(TEMPERMENT, new ResourceLocation("ke2:temperment"));
+		registerPotionMix(PotionTypes.AWKWARD, Ke2Items.TEMPERPLASM, TEMPERMENT);
 	}
-	public static void registerPotion(Potion effect, ResourceLocation name) {
-		ForgeRegistries.POTIONS.register(effect.setRegistryName(name));
+	public static void registerPotion(Potion potion, ResourceLocation name) {
+		ForgeRegistries.POTIONS.register(potion.setRegistryName(name));
+	}
+	public static void registerPotionType(PotionType type, ResourceLocation name) {
+		ForgeRegistries.POTION_TYPES.register(type.setRegistryName(name));
+	}
+	public static void registerPotionMix(PotionType start, Item modifier, PotionType output) {
+		PotionHelper.addMix(start, modifier, output);
 	}
 }
--- src/main/java/mod/ke2/potion/PotionTempermental.java
+++ src/main/java/mod/ke2/potion/PotionTempermental.java
@@ -1,28 +1,22 @@
 package mod.ke2.potion;
 
+import mod.ke2.init.Ke2Potions;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PotionTempermental extends Potion {
+	public static final PotionType TEMPERMENT      = new PotionType(new PotionEffect[] { new PotionEffect(Ke2Potions.TEMPERMENTAL, 7200, 1) });
 	public PotionTempermental() {
 		super(true, 0x438197);
 		this.setPotionName("effect.tempermental");
 		this.setIconIndex(-1, -1);
 		this.setBeneficial();
-	}
-	@Override
-	public void performEffect(EntityLivingBase afflicted, int amplifier) {
-		
-    }
-	@Override
-	public boolean hasStatusIcon() {
-		return false;
 	}
 	@Override
 	@SideOnly(Side.CLIENT)
