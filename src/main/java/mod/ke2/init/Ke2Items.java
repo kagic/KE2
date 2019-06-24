eb2d2c93d64a8533139bb00e4e792134352e1c93
Owen Adrian <akrivus@gmail.com>
2019-06-23 23:55:02 -0500

    Removed Dragonsteel arrows.

--- src/main/java/mod/ke2/init/Ke2Items.java
+++ src/main/java/mod/ke2/init/Ke2Items.java
@@ -2,7 +2,6 @@ package mod.ke2.init;
 
 import java.util.Iterator;
 
-import mod.ke2.entity.machine.EntityTungstenArrow;
 import mod.ke2.item.ItemEssence;
 import mod.ke2.item.ItemGemDestabilizer;
 import mod.ke2.item.ItemGemDust;
@@ -15,15 +14,12 @@ import mod.ke2.item.ItemWarpWhistle;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
-import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
@@ -31,23 +27,15 @@ import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.oredict.OreDictionary;
 
 public class Ke2Items {
 	public static final ArmorMaterial TUNGSTEN_ARMOR = EnumHelper.addArmorMaterial("TUNGSTEN_ARMOR", "ke2:tungsten", 24, new int[]{4, 8, 8, 4}, 6, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 3.0F).setRepairItem(new ItemStack(Ke2Items.TUNGSTEN_INGOT));
 	public static final ToolMaterial TUNGSTEN_TOOLS = EnumHelper.addToolMaterial("TUNGSTEN", 3, 909, 7.0F, 2.0F, 9);
-	public static class TungArrow extends ItemArrow {
-		@Override
-		public EntityArrow createArrow(World world, ItemStack stack, EntityLivingBase shooter) {
-	        return new EntityTungstenArrow(world, shooter);
-	    }
-	}
 	public static class TungPickaxe extends ItemPickaxe {
 		protected TungPickaxe() {
 			super(Ke2Items.TUNGSTEN_TOOLS);
@@ -78,7 +66,6 @@ public class Ke2Items {
 	public static final Item TUNGSTEN_SPADE = new ItemSpade(Ke2Items.TUNGSTEN_TOOLS).setUnlocalizedName("tungsten_spade");
 	public static final Item TUNGSTEN_PICKAXE = new TungPickaxe().setUnlocalizedName("tungsten_pickaxe");
 	public static final Item TUNGSTEN_AXE = new TungAxe().setUnlocalizedName("tungsten_axe");
-	public static final Item TUNGSTEN_ARROW = new TungArrow().setUnlocalizedName("tungsten_arrow");
 	public static final ItemGemStaff GEM_STAFF = new ItemGemStaff();
 	public static final ItemWarpWhistle WARP_WHISTLE = new ItemWarpWhistle();
 	public static final ItemGemDestabilizer GEM_DESTABILIZER = new ItemGemDestabilizer();
@@ -114,7 +101,6 @@ public class Ke2Items {
 		Ke2Items.registerItem(Ke2Items.TUNGSTEN_SPADE, new ResourceLocation("ke2:tungsten_spade"), event, Ke2CreativeTabs.GEM_TECH);
 		Ke2Items.registerItem(Ke2Items.TUNGSTEN_PICKAXE, new ResourceLocation("ke2:tungsten_pickaxe"), event, Ke2CreativeTabs.GEM_TECH);
 		Ke2Items.registerItem(Ke2Items.TUNGSTEN_AXE, new ResourceLocation("ke2:tungsten_axe"), event, Ke2CreativeTabs.GEM_TECH);
-		Ke2Items.registerItem(Ke2Items.TUNGSTEN_ARROW, new ResourceLocation("ke2:tungsten_arrow"), event, Ke2CreativeTabs.GEM_TECH);
 		Ke2Items.registerItem(Ke2Items.TUNGSTEN_INGOT, new ResourceLocation("ke2:tungsten_ingot"), event, Ke2CreativeTabs.GEM_TECH);
 		
 		Iterator<Block> blocks = Ke2Blocks.ITEMS.keySet().iterator();
